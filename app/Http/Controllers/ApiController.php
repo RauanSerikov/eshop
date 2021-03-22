448f2df8adf69b9010167461d569f1aef7fdb5c5
Rauan <190103349@stu.sdu.edu.kz>
2021-03-22 15:59:04 +0600

    created two post APIs

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class ApiController extends Controller
+{
+    /*
+        Function returns all post rows
+    */
+    public function index(Request $request) {
+        $posts = Post::all();
+
+        return response($posts, 200);
+    }
+
+    /*
+        Return post with postid
+    */
+    public function get_post(Request $request){
+        $post = Post::find($request->post_id);
+
+        if ($post == null){
+            return response(['message' => 'There is no post'], 404);
+        }
+
+        return response($post, 200);
+    }
+}
+    
